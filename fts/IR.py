ac720ce9ce7be1eba82cbb6d4b312cac7f135e15
Marsel Gabdulkhanov <gabmars96@yandex.ru>
2019-04-08 11:26:11 +0500

    Add files via upload

--- fts/IR.py
+++ fts/IR.py
@@ -4,10 +4,11 @@ from PyQt5 import uic, QtWidgets, QtCore, QtGui
 import psycopg2
 import pandas as pd
 import sqlite3
-
+import time
 import lucene
 import os
-import resources
+import logging
+from IR_PandasModel import PandasModel
 
 from java.nio.file import Paths
 from org.apache.lucene.analysis.standard import StandardAnalyzer
@@ -20,111 +21,100 @@ Qt = QtCore.Qt
 PATH = ''
 LIMIT = 10
 
+logging.basicConfig(filename="search.log", level=logging.INFO)
 
 
-class PandasModel(QtGui.QStandardItemModel):
-    def __init__(self, data, parent=None):
-        QtGui.QStandardItemModel.__init__(self, parent)
-        self._data = data
-
-        for row in data.values.tolist():
-            data_row = [QtGui.QStandardItem(x) for x in row]
-            self.appendRow(data_row)
-        return
-
-    def rowCount(self, parent=None):
-        return len(self._data.values)
-
-    def columnCount(self, parent=None):
-        return self._data.columns.size
-
-    def headerData(self, x, orientation, role):
-        if orientation == QtCore.Qt.Horizontal and role == QtCore.Qt.DisplayRole:
-            return self._data.columns[x]
-        if orientation == QtCore.Qt.Vertical and role == QtCore.Qt.DisplayRole:
-            return self._data.index[x]
-        return None
-
-
-class mywindow(QtWidgets.QMainWindow):
+class MyWindow(QtWidgets.QMainWindow):
     def __init__(self):
-        super(mywindow, self).__init__()
-
+        super(MyWindow, self).__init__()
 
         self.ui = uic.loadUi(PATH + "/design.ui", self)
+        self.statusBar().showMessage('Ready')
 
-        self.ui.radioButton_2.setEnabled(False)
+        self.ui.radioButton_SQLite.setEnabled(False)
 
-        self.ui.pushButton.clicked.connect(self.btnClicked)
-        self.ui.comboBox.currentTextChanged.connect(self.onChange)
+        self.ui.pushButton_search.clicked.connect(self.button_search_clicked)
+        self.ui.comboBox.currentTextChanged.connect(self.on_change)
 
         self._layout = self.layout()
-        self.movie = QtGui.QMovie(':/images/spinner.gif', QtCore.QByteArray(), self)
-        self.movie_screen = QtWidgets.QLabel()
-        self.movie_screen.setAlignment(Qt.AlignCenter)
-        self.ui.verticalLayout.addWidget(self.movie_screen)
-        self.setLayout(self.ui.verticalLayout)
-        self.movie_screen.setMovie(self.movie)
-        self.movie.start()
-        self.movie.loopCount()
 
-    def btnClicked(self):
-
-        substring = self.ui.lineEdit.text()
+    def button_search_clicked(self):
+        t1 = time.time()
+        title_substring = self.ui.lineEdit_title.text()
         mode = self.ui.comboBox.currentText()
-        if not self.ui.radioButton_3.isChecked():
+
+        # SQLite/PostgreSQL
+        if not self.ui.radioButton_Lucene.isChecked():
             if mode == 'Полное совпадение':
-                query_string = """name ilike '{}'""".format(substring)
+                query_string = """name ilike '{}'""".format(title_substring)
+
             if mode == 'Частичное совпадение':
-                query_string = """name ilike '%{}%'""".format(substring)
+                query_string = """name ilike '%{}%'""".format(title_substring)
+
             if mode == 'Частичное совпадение по словам':
-                query_string = ' or '.join(["""name ilike '%{}%'""".format(ss) for ss in substring.split()])
+                query_string = ' or '.join(["""name ilike '%{}%'""".format(ss) for ss in title_substring.split()])
+
             if mode == 'Полное совпадение + Год':
-                year_substring = self.ui.lineEdit_2.text()
+                year_substring = self.ui.lineEdit_year.text()
                 year_substring = '= ' + year_substring if year_substring != '' else ' is null'
-                query_string = """name ilike '{}' and year {}""".format(substring, year_substring)
+                query_string = """name ilike '{}' and year {}""".format(title_substring, year_substring)
+
             if mode == 'Частичное совпадение + Год':
-                year_substring = self.ui.lineEdit_2.text()
+                year_substring = self.ui.lineEdit_year.text()
                 year_substring = '= ' + year_substring if year_substring != '' else ' is null'
-                query_string = """name ilike '%{}%' and year {}""".format(substring, year_substring)
+                query_string = """name ilike '%{}%' and year {}""".format(title_substring, year_substring)
+
             if mode == 'Частичное совпадение по словам + Год':
-                year_substring = self.ui.lineEdit_2.text()
+                year_substring = self.ui.lineEdit_year.text()
                 year_substring = '= ' + year_substring if year_substring != '' else ' is null'
-                query_string = ' or '.join(["""name ilike '%{}%'""".format(ss) for ss in substring.split()])
+                query_string = ' or '.join(["""name ilike '%{}%'""".format(ss) for ss in title_substring.split()])
                 query_string = '({}) and year {}'.format(query_string,year_substring)
 
-            if self.ui.radioButton.isChecked():
-                con = psycopg2.connect(user='developer', password='rtfP@ssw0rd', host='84.201.147.162',
-                                       dbname='information_retrieval')
+            # PostgreSQL connection
+            if self.ui.radioButton_PostgreSQL.isChecked():
+                con = psycopg2.connect(user='', password='', host='',
+                                       dbname='')
+                se = 'PostgreSQL'
+
+            # SQLite connection
             else:
                 con = sqlite3.connect(PATH + '/imdb.db')
+                se = 'SQLite'
+
             df = pd.read_sql('select * from movies where ' + query_string, con).head(LIMIT)
             con.close()
             df = df.fillna('').astype(str)
             df['year'] = df['year'].apply(lambda x: x.replace('.0', ''))
+
+        # Lucene
         else:
             lucene.initVM()
-            indexDir = SimpleFSDirectory(Paths.get('index'))
-            reader = DirectoryReader.open(indexDir)
+            index_dir = SimpleFSDirectory(Paths.get('index'))
+            reader = DirectoryReader.open(index_dir)
             searcher = IndexSearcher(reader)
-
+            query_string = ''
+            se = 'Lucene'
             if mode == 'Полное совпадение':
-                query_string = 'name:"{}"'.format(substring)
-            if mode == 'Частичное совпадение':
-                query_string = 'name:{}'.format(substring)
-            if mode == 'Частичное совпадение по словам':
-                query_string = ' or '.join(["""name:{}""".format(ss) for ss in substring.split()])
-            if mode == 'Полное совпадение + Год':
-                year_substring = self.ui.lineEdit_2.text()
-                query_string = 'name:"{}" AND year:"{}"'.format(substring, year_substring)
-            if mode == 'Частичное совпадение + Год':
-                year_substring = self.ui.lineEdit_2.text()
-                query_string = 'name:{} AND year:"{}"'.format(substring, year_substring)
-            if mode == 'Частичное совпадение по словам + Год':
-                year_substring = self.ui.lineEdit_2.text()
-                query_string = ' or '.join(["""name:{}""".format(ss) for ss in substring.split()])
-                query_string = '({}) and year:"{}"'.format(query_string, year_substring)
+                query_string = 'name:"{}"'.format(title_substring)
 
+            if mode == 'Частичное совпадение':
+                query_string = 'name:{}'.format(title_substring)
+
+            if mode == 'Частичное совпадение по словам':
+                query_string = ' or '.join(["""name:{}""".format(ss) for ss in title_substring.split()])
+
+            if mode == 'Полное совпадение + Год':
+                year_substring = self.ui.lineEdit_year.text()
+                query_string = 'name:"{}" AND year:"{}"'.format(title_substring, year_substring)
+
+            if mode == 'Частичное совпадение + Год':
+                year_substring = self.ui.lineEdit_year.text()
+                query_string = 'name:{} AND year:"{}"'.format(title_substring, year_substring)
+
+            if mode == 'Частичное совпадение по словам + Год':
+                year_substring = self.ui.lineEdit_year.text()
+                query_string = ' or '.join(["""name:{}""".format(ss) for ss in title_substring.split()])
+                query_string = '({}) and year:"{}"'.format(query_string, year_substring)
 
             query = QueryParser("defaultField", StandardAnalyzer()).parse(query_string)
             hits = searcher.search(query, LIMIT)
@@ -136,20 +126,28 @@ class mywindow(QtWidgets.QMainWindow):
             if not df.empty:
                 df.columns = ['id', 'name', 'year']
 
-        model = PandasModel(df)
-        self.tableView.setModel(model)
+        pandas_model = PandasModel(df)
+        self.tableView.setModel(pandas_model)
+        self.tableView.horizontalHeader().setSectionResizeMode(1)
 
-    def onChange(self, value):
+        t2 = time.time()
+        self.statusBar().showMessage('Searched [{}] with {} for {} s'.format(query_string, se, str(t2 - t1)))
+
+        logging.info('Searched [{}] with {} for {} s'.format(query_string, se, str(t2 - t1)))
+        logging.info(df)
+        logging.info('---------------------------------------------------------')
+
+    def on_change(self, value):
         if 'Год' in value:
-            self.ui.lineEdit_2.setEnabled(True)
+            self.ui.lineEdit_year.setEnabled(True)
         else:
-            self.ui.lineEdit_2.setEnabled(False)
+            self.ui.lineEdit_year.setEnabled(False)
 
 
 if __name__ == '__main__':
     PATH = os.getcwd()
     app = QApplication([])
-    application = mywindow()
+    application = MyWindow()
     application.show()
 
     sys.exit(app.exec_())
--- fts/README.md
+++ fts/README.md
@@ -1,13 +1,17 @@
 # Files descriptions
 
-**IR.py - файл, реализующий приложение с интерфейсом поиска (основной)**
+**[IR.py](IR.py) - файл, реализующий приложение с интерфейсом поиска (основной)**
 
-IR_load.py - скрипт загрузки данных в базу данных PostgreSQL + Sqlite
+[IR_load.py](IR_load.py) - скрипт загрузки данных в базу данных PostgreSQL + Sqlite
 
-IR_lucene_indexer.py - скрипт индексирования данных
+[IR_lucene_indexer.py](IR_lucene_indexer.py) - скрипт индексирования данных
 
-IR_lucene_retriever.py - тестовый скрипт поиска данных по индексу
+[IR_lucene_retriever.py](IR_lucene_retriever.py) - тестовый скрипт поиска данных по индексу
 
-check.py - проверка зависимостей
+[check.py](check.py) - проверка зависимостей
 
-resources.py - файл с ресурсами интерфейса
+[resources.py](resources.py) - файл с ресурсами интерфейса
+
+[IR_PandasModel.py](IR_PandasModel.py)- модель **M**VC для отображения pandas dataframe 
+
+[search.log](search.log) - лог поиска
