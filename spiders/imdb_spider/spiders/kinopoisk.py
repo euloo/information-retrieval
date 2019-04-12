3ac44c5a63c700c202b10a2250d8aa445de7d29f
Marsel Gabdulkhanov <gabmars96@yandex.ru>
2019-04-12 23:42:59 +0500

    Add files via upload

--- spiders/imdb_spider/spiders/kinopoisk.py
+++ spiders/imdb_spider/spiders/kinopoisk.py
@@ -16,24 +16,24 @@ class KinopoiskSpider(scrapy.Spider):
         info={}
         for tr in response.css('#infoTable>table').css('tr'):
             tds=tr.css('td')
-            info[' '.join(tds[0].xpath('text()').extract()).strip()]=' '.join(tds[1].xpath('.//text()').extract()).strip()
+            info[' '.join(tds[0].xpath('text()').get()).strip()]=' '.join(tds[1].xpath('.//text()').get()).strip()
         
-        yield {'year': info['год']}
-        yield {'release_date': info['премьера (мир)']}
-        yield {'genres': info['жанр']}
-        yield {'director': info['режиссер']}
+        yield {'year': info.get('год', '')}
+        yield {'release_date': info.get('премьера (мир)', '')}
+        yield {'genres': info.get('жанр', '')}
+        yield {'director': info.get('режиссер', '')}
         
-        yield {'title':response.css('#headerFilm > h1').xpath('text()').extract()}
-        yield {'en_title':response.css('#headerFilm > span').xpath('text()').extract()}
+        yield {'title':response.css('#headerFilm > h1').xpath('text()').get()}
+        yield {'en_title':response.css('#headerFilm > span').xpath('text()').get()}
         
         top_3_cast=[]
         for i, li in enumerate(response.css('li[itemprop="actors"]>a[href*="name"]')):
-            top_3_cast.append(li.xpath('text()').extract())
+            top_3_cast.append(li.xpath('text()').get())
             if i == 2:
                 break
         yield {'top_3_cast':top_3_cast}
 
-        yield {'storyline':response.css('div[itemprop="description"]').xpath('text()').extract()}
+        yield {'storyline':response.css('div[itemprop="description"]').xpath('text()').get()}
         
         for kws in response.css('a.wordLinks[href*="keywords"]'):
             yield response.follow(kws, self.parseKeywords)
@@ -41,5 +41,5 @@ class KinopoiskSpider(scrapy.Spider):
     def parseKeywords(self, response):
         keywords=[]
         for kwds in response.css('a[href*="lists/m_act%5Bkeyword%5D"]'):
-            keywords.append(kwds.xpath('text()').extract())
+            keywords.append(kwds.xpath('text()').get())
         yield {'keywords': keywords}
