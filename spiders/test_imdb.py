6841162d62b0822dd92231bc95af3598a00a1717
Marsel Gabdulkhanov <gabmars96@yandex.ru>
2019-04-12 23:42:38 +0500

    Add files via upload

--- spiders/test_imdb.py
+++ spiders/test_imdb.py
@@ -8,7 +8,7 @@ class ImdbSpider(scrapy.Spider):
     start_urls=['https://www.imdb.com/title/tt4154756']
     
     def parse(self, response):
-        yield {'id': re.findall('https://www.imdb.com/title/tt(\d*)/',response.url)}
+        yield {'id': re.findall('https://www.imdb.com/title/tt(\d*)/',response.url)[0]}
         
         for title in response.css('.title_wrapper>h1'):
             yield {'title': title.css('h1 ::text').get().replace(u'\xa0', u'')}
@@ -16,24 +16,23 @@ class ImdbSpider(scrapy.Spider):
 
         for rating in response.css('.ratingValue>strong>span'):
             yield {'rating': rating.css('span ::text').get()}
-            yield {'ratingXPath': rating.xpath('string(.)').extract()}
 
         directors=[]
         for creds in response.css('a[href*="tt_ov_dr"]'):
-            directors.append(creds.xpath('text()').extract())
+            directors.append(creds.xpath('text()').get())
         yield {'director': directors}
 
         genres=[]
         for genre in response.css('div.subtext>a[href*="genres"]'):
-            genres.append(genre.xpath('text()').extract())
+            genres.append(genre.xpath('text()').get())
         yield {'genres': genres}
 
         for rating in response.css('#titleStoryLine > div:nth-child(3) > p > span'):
-            yield {'storyline': rating.xpath('text()').extract()}
+            yield {'storyline': rating.xpath('text()').get()}
               
         top_3_cast=[]
         for i, cast in enumerate(response.css('#titleCast>table').css('a[href*="name"][href*="/?ref_=tt_cl_t"]')):
-            top_3_cast.append(cast.xpath('text()').extract())
+            top_3_cast.append(cast.xpath('text()').get())
             if i == 2:
                 break
         yield {'top_3_cast': top_3_cast}
@@ -49,20 +48,20 @@ class ImdbSpider(scrapy.Spider):
         
     def parseSyno(self, response):
         for syno in response.css('#plot-synopsis-content>li'):
-            yield {'synopsys': syno.xpath('text()').extract()}
+            yield {'synopsys': syno.xpath('text()').get()}
             
     def parseKeywords(self, response):
         kwds=[]
         for keyword in response.css('div.sodatext>a'):
-            kwds.append(keyword.xpath('text()').extract())
+            kwds.append(keyword.xpath('text()').get())
         yield {'keywords':kwds}
     
     def parseReleaseDate(self, response):
         rd=[]
         for tr in response.css('tr.ipl-zebra-list__item.release-date-item'):
-            a=tr.css('a').xpath('text()').extract()
-            tds=[td.xpath('text()').extract() for td in tr.css('td')]
-            rd.append([a[0], ' '.join(itertools.chain.from_iterable(tds))])
+            a=tr.css('a').xpath('text()').get()
+            tds=[td.xpath('text()').get() for td in tr.css('td') if td.xpath('text()').get()]
+            rd.append([a, ' '.join(tds)])
         yield {'releasedates':rd}
 
 from scrapy import signals
